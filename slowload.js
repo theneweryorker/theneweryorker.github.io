5e9fed1c59052b6dcb5d4f40e9a4399396d9ea5e
Holly Li <hollyli@hollyli-macbookpro.roam.corp.google.com>
2020-05-01 11:36:01 -0700

    message

---
+++ slowload.js
@@ -0,0 +1,27 @@
+
+
+
+$(document).ready(function(){
+
+	//
+	// Set each project span to 0 opacity and animate to fully opaque. 
+	// Used a series of delay methods to craete cascade effect
+	//
+
+	$("#line1").delay(100).animate( {opacity: 1 } , 700);  				
+			
+	$("#line2").delay(200).animate( {opacity: 1 } , 700);
+
+	$("#line3").delay(300).animate( {opacity: 1 } , 700);
+	
+	$("#line4").delay(400).animate( {opacity: 1 } , 700);
+	
+	$("#line5").delay(500).animate( {opacity: 1 } , 700);
+	
+	$("#line6").delay(600).animate( {opacity: 1 } , 700);
+	
+	$("#line7").delay(700).animate( {opacity: 1 } , 700);
+
+});
+	
+		
